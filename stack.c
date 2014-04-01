f11c9cc2868d755294ad563473ae926a8faae550
Kodai <kodaitakahashi.kt@gmail.com>
2014-04-01 17:16:46 +0900

    Registration

---
+++ stack.c
@@ -0,0 +1,38 @@
+#difine STACK_MAX 10
+
+/*double型のデータを格納するスタックを作成*/
+double stack[STACK_MAX];
+/*スタック頂上の位置（最下部からのオフセット) */
+int stack_top=0;
+
+/*スタックへpush*/
+void stack_push(double val){
+  if (stack_top == STACK_MAX){
+    /*スタックが満杯になっている */
+    printf("エラー：スタックが満杯です(stack overflow)\n");
+    exit(EXIT_FAILURE);
+  }
+  else
+    {
+      /*渡された値をスタックに積む */
+      stack[stack_top]=val;
+      ++stack_top;
+    }
+}
+
+/*スタックからデータをpop */
+double stack_pop(void){
+  if(stack_top == 0){
+    /*スタックには何もない */
+    printf("エラー:スタックが空なのにpopが呼ばれました""(stack underflor)\n");
+	   exit(EXIT_FAILURE);
+
+	   return 0;
+  }
+  else
+    {
+      /*一番上の値を返す */
+      --stack_top;
+      return stack[stack_pop];
+    }
+}

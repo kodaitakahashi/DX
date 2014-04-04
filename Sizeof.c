f450243f945d6c39f8234ca9353edd967bd4bcf6
Kodai <kodaitakahashi.kt@gmail.com>
2014-04-04 21:41:51 +0900

    Sizeof

---
+++ Sizeof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void){
+  printf("Size of int: %ubyte\n",sizeof(int)); /* 4byte */
+  printf("Size of char: %ubyte\n",sizeof(char)); /* 1byte */ 
+  printf("Size of double: %ubyte\n",sizeof(double)); /* 8byte */ 
+  printf("Size of float: %ubyte\n",sizeof(float)); /* 4byte */
+
+}
+

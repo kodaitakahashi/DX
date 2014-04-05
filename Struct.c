898bfd385068ea1012279a75fbda3d7e279bd57a
Kodai <kodaitakahashi.kt@gmail.com>
2014-04-05 20:48:34 +0900

    UPDATE

--- Struct.c
+++ Struct.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/*　構造体の型枠の宣言 */
-struct seiseki {
+/* 構造体の型枠の宣言 */
+typedef struct {
   int no;         /* 学生番号 */
   char name[20];  /* 氏名 */
   double average; /* 平均値 */
-};
+} SEISEKI ;
 
 int main(void){
   int i;
-  /* 構造体の宣言　*/
+  /* 構造体の宣言 */
   /* 構造体の初期化 */
-  struct seiseki seito1 = {5,"KASAHARA", 83.5};
-  struct seiseki seito2[20] = {
+  SEISEKI seito1 = {5,"KASAHARA", 83.5};
+  SEISEKI seito2[20] = {
     {1,"SAKURAI",78.6},
     {2,"NAGANO",57.3},
     {3,"TAKESHITA",66.4},
@@ -25,3 +25,8 @@ int main(void){
 
   return 0;
 }
+
+/* typedefを使用することによって、構造体の処理が簡潔な書き方ができる。
+   typedefがない場合は、構造体を使うときにstructが頭に付いてしまい、長くなる。
+   しかし、使用することによって、structが頭に付けなくてもいい。 */
+

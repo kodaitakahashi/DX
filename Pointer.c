db9adb1752c0667fd88f04267033f26a572258fd
Kodai <kodaitakahashi.kt@gmail.com>
2014-04-05 17:48:37 +0900

    UPDATE

--- Pointer.c
+++ Pointer.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-void main(void) {
+int main(void) {
   int *p=NULL,a=10; 
   p = &a; /* pにaのメモリアドレスを格納する。 */
   printf("中身 %d,アドレス %p\n",*p,&p); /* 中身 10,アドレス 0xbf971ffc */
+
+  return 0;
 }
+
--- Sizeof.c
+++ Sizeof.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void main(void){
+int main(void){
   printf("Size of int: %ubyte\n",sizeof(int)); /* 4byte */
   printf("Size of char: %ubyte\n",sizeof(char)); /* 1byte */ 
   printf("Size of double: %ubyte\n",sizeof(double)); /* 8byte */ 
   printf("Size of float: %ubyte\n",sizeof(float)); /* 4byte */
 
+  return 0;
 }
 

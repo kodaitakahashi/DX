cf61338177c75cac5cbddf9327ee761b36ab5abc
Kodai <kodaitakahashi.kt@gmail.com>
2014-04-17 21:23:35 +0900

    UPDATE

--- Node_stack.c
+++ Node_stack.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef int date_t;
+typedef int data_t;
 
 typedef struct nodetag{
-  date_t date;
+  data_t data;
   struct nodetag *next;
 } node_t;
 
-node_t *push(node_t *stackPt , date_t date);//スタックの中身に値を入れる
-node_t *pop(node_t *stackPt , date_t *date); //スタックの中身から一番上の値を抜く
+node_t *push(node_t *stackPt , data_t data);//スタックの中身に値を入れる
+
+node_t *pop(node_t *stackPt , data_t *data); //スタックの中身から一番上の値を抜く
+
 int Empty(node_t *stackPt);// スタックの中身が空の時に使用
+
 void print_stack(node_t *stackPt ); //スタックの中身を表示
+
 void usage(void);//メニュー表示
 
 int main(void){
   node_t *stackPt = NULL;
-  date_t date;
+  data_t data;
   int menu;
 
   usage();
@@ -26,14 +30,14 @@ int main(void){
     switch(menu){
     case 1:
       printf("スタックにpushする整数を入力してください:");
-      scanf("%d",&date);
-      stackPt = push(stackPt,date);
+      scanf("%d",&data);
+      stackPt = push(stackPt,data);
       print_stack(stackPt);
       break;
     case 2:
       if (!Empty(stackPt)){
-        stackPt = pop(stackPt,&date);
-        printf("popした値は%dです\n",date);
+        stackPt = pop(stackPt,&data);
+        printf("popした値は%dです\n",data);
       }
       break;
     case 3:
@@ -63,11 +67,11 @@ int Empty(node_t *stackPt){
   return stackPt == NULL;
 }
 
-node_t *push(node_t *topPt,date_t date){
+node_t *push(node_t *topPt,data_t data){
   node_t *newPt; //新規ノード作成
   newPt = (node_t *)malloc(sizeof(node_t)); //メモリ確保
   if(newPt != NULL){
-    newPt->date = date; //新規ノードに挿入する値を代入
+    newPt->data = data; //新規ノードに挿入する値を代入
     newPt->next = topPt;//新規ノードにtopPt(stackPt)のアドレスを代入
     topPt = newPt; //topPtに新規ノードのアドレスを代入。この操作によりtopPtが新たなデータを指すようになる。
   }else{
@@ -76,10 +80,10 @@ node_t *push(node_t *topPt,date_t date){
   return topPt;
 }
 
-node_t *pop(node_t *topPt,date_t *pop_value){
+node_t *pop(node_t *topPt,data_t *pop_value){
   node_t *tmpPt;
   tmpPt = topPt;
-  *pop_value =topPt->date; //*pop_value(*date)にtopPtのdateを代入 
+  *pop_value =topPt->data; //*pop_value(*data)にtopPtのdataを代入 
   topPt = topPt -> next;//topPtにtopPtのnextに代入
   free (tmpPt);//tmpPt(topPt）のメモリを開放
   return topPt;
@@ -91,7 +95,7 @@ void print_stack(node_t *stackPt){
     printf("スタックの中身が空です\n");
   }else{
     while(stackPt != NULL){
-      printf("[%d",stackPt -> date);
+      printf("[%d",stackPt -> data);
       printf("]\n");
       stackPt = stackPt->next;
     }
